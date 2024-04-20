3c01f5e378fa906ba6914c52f793dc28affe40b3
OTHMAN-BENMALEK <159661363+OTHMAN-BENMALEK@users.noreply.github.com>
2024-04-20 15:47:45 +0100

    Add files via upload

---
+++ Part 1/src/main/java/ma/fs/hospital/HospitalApplication.java
@@ -0,0 +1,29 @@
+package ma.fs.hospital;
+
+import ma.fs.hospital.entities.Patient;
+import ma.fs.hospital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class HospitalApplication implements CommandLineRunner {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalApplication.class, args);
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepository.save(new Patient(null,"Othman",new Date(),false,36));
+        patientRepository.save(new Patient(null,"Jubil",new Date(),false,5130));
+        patientRepository.save(new Patient(null,"Moha",new Date(),true,147));
+
+
+    }
+}
